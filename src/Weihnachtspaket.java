beb8b166b08ea1e19c1021b7ba6fbf932114b89e
meberlei <matthias_leonhard.eberlein@smail.th-koeln.de>
2019-10-31 11:57:30 +0100

    Weihnachtspaket gefixt

--- src/Weihnachtspaket.java
+++ src/Weihnachtspaket.java
@@ -1,8 +1,5 @@
 public class Weihnachtspaket implements Geschenkpaket {
 
-    private Werbegeschenk geschenk;
-    private Gutschein gutschein;
-
     public Gutschein erzeugeGutschein() {
         Gutschein gutschein = new CD(20);
         return gutschein;
